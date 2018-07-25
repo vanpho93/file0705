94ed8366b63f223f2e40f53a5a00ba87b42fa0c8
Van Pho Nguyen <vanpho02@gmail.com>
2018-07-25 20:22:26 +0700

    init

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
+const multer = require('multer');
+const fs = require('fs');
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, Date.now() + '.png')
+});
+
+const upload = multer({ storage });
 
 app.use(express.static('./public'));
 
-app.post('/avatar', (req, res) => {
-    res.send({ success: true, message: 'Avatar changed.', url: 'abcd.png' });
+app.post('/avatar', upload.single('image'), (req, res) => {
+    res.send({ success: true, message: 'Avatar changed.', url: req.file.filename });
 });
 
-app.listen(3000, () => console.log('Server started!'));
+app.get('/files', (req, res) => {
+    res.send({ success: true, files: fs.readdirSync('./public') });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started!'));
